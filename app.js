d229262961a1339da8022d6951dad1f110e8aa43
rachelharu <rachel.haru9@gmail.com>
2022-01-18 14:42:14 -0800

    set up get and put request for a specific article

--- app.js
+++ app.js
@@ -26,33 +26,69 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
-app.get("/articles", (req, res) => {
-  //modelname, conditions, results
-  //if then statement to show errors or results
-  Article.find((err, foundArticles) => {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+///////requests targeting all articles /////////
 
-  });
+app.route("/articles")
+.get((req, res) =>{
+//modelname, conditions, results
+//if then statement to show errors or results
+ Article.find((err, foundArticles) => {
+  if (!err) {
+    res.send(foundArticles);
+  } else {
+    res.send(err);
+  }
+
 });
-
-app.post("/articles", (req, res) => {
+})
+.post((req, res) => {
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
   });
-  newArticle.save( (err){
+  newArticle.save((err) =>{
     if (!err){
       res.send("Successfully added a new article.")
     } else {
       res.send(err);
     }
   });
+})
+.delete((req, res) => {
+  Article.deleteMany((err) =>{
+    if(!err){
+      res.send("Successfully deleted all articles");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
+///////requests targeting specific articles /////////
+
+app.route("/articles/:articleTitle")
+.get((req, res) => {
+  //looks thru collection of articles find ONE document where title is = to req.params
+  Article.findOne({title:req.params.articleTitle }, (err, foundArticle) => {
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+.put((req, res) => {
+  Article.replaceOne(
+    {title:req.params.articleTitle},
+    {title:req.body.title, content: req.body.content},
+    {overwrite: true},
+    (err, )=>{
+      if (!err){
+        res.send("Successfully updated article.")
+      }
+    }
+  );
+});
 
 
 
